c22ba14ca3559ef5d1d0c23286021a8346ed3f78
Benjamin Rosas <ben@aliencyb.org>
2021-01-26 14:58:01 -0600

    install theme-ui

---
+++ theme.js
@@ -0,0 +1,14 @@
+import { deep } from '@theme-ui/presets'
+
+const theme = {
+  ...deep,
+  containers: {
+    card: {},
+    page: {}
+  },
+  styles: {
+    ...deep.styles
+  }
+}
+
+export default theme
