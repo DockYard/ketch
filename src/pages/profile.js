5a900f3913b3b1646afa858f86e048d7d315765d
Benjamin Rosas <ben@aliencyb.org>
2021-02-19 16:21:21 -0600

    Profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,7 +2,32 @@
 /** @jsx jsx */
 
 import { jsx } from 'theme-ui'
+import { useAuth } from 'hooks/useAuth'
+import Link from 'next/link'
+import styles from 'pages/profile.module.scss'
 
-const Profile = () => <h1>Profile</h1>
+const Profile = () => {
+  const auth = useAuth()
+
+  if (!auth.user)
+    return (
+      <div className={styles.profile}>
+        <h1>Profile</h1>
+        <p>You are not loggeed in.</p>
+        <p>
+          Please <Link href="/login">Log in</Link> or
+          <Link href="/register"> Register</Link>
+        </p>
+      </div>
+    )
+
+  return (
+    <div className={styles.profile}>
+      <h1>Profile</h1>
+      <h2>{`Welcome ${auth.user.name}`}</h2>
+      <p>{`You are logged in with ${auth.user.email}`}.</p>
+    </div>
+  )
+}
 
 export default Profile
